a510bdbb1036a177579c15510ae7f375f860d50b
Octaver <Octaver2131@outlook.com>
2025-06-16 10:22:06 +0800

    List组件

--- src/pages/MyChart/index.tsx
+++ src/pages/MyChart/index.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { listMyChartByPageUsingPOST } from '@/services/yubi/chartController';
-import { Avatar, List, message } from 'antd';
+import { Avatar, Card, List, message } from 'antd';
 import ReactECharts from 'echarts-for-react';
+import { useModel } from '@@/exports';
 
 
 /**
@@ -15,10 +16,14 @@ const MyChartPage: React.FC = () => {
   }
 
   const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({...initSearchParams});
+  const { initialState } = useModel('@@initialState');
+  const { currentUser } = initialState ?? {};
   const [chartList, setChartList] = useState<API.Chart[]>();
   const [total, setTotal] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const logData = async() => {
+    setLoading(true);
     try {
       const res = await listMyChartByPageUsingPOST(searchParams);
       if (res.data) {
@@ -30,7 +35,8 @@ const MyChartPage: React.FC = () => {
     } catch (e: any) {
       message.error(' 获取我的数据失败.' + e.message);
     }
-  }
+    setLoading(false);
+  };
 
   useEffect(() => {
     logData();
@@ -40,13 +46,15 @@ const MyChartPage: React.FC = () => {
     <div className="my-chart-page">
       <List
         itemLayout="vertical"
+        grid = {{ gutter: 16, column: 2 }}
         size="large"
         pagination={{
           onChange: (page) => {
             console.log(page);
           },
-          pageSize: 3,
+          pageSize: searchParams.pageSize,
         }}
+        loading={loading}
         dataSource={chartList}
         footer={
           <div>
@@ -56,17 +64,17 @@ const MyChartPage: React.FC = () => {
         renderItem={(item) => (
           <List.Item
             key={item.id}
-
-            extra={
-              <ReactECharts option={JSON.parse(item.genChart ?? '{}')} />
-            }
           >
-            <List.Item.Meta
-              avatar={<Avatar src={'https://api.dicebear.com/7.x/miniavs/svg?seed=1'} />}
-              title={item.name}
-              description={item.chartType ? ('图表类型：' + item.chartType) : undefined }
-            />
-            {'分析目标：' + item.goal}
+            <Card>
+              <List.Item.Meta
+                avatar={<Avatar src={currentUser && currentUser.userAvatar} />}
+                title={item.name}
+                description={item.chartType ? ('图表类型：' + item.chartType) : undefined }
+              />
+              {'分析目标：' + item.goal}
+              <div style={{ marginBottom: 16 }}/>
+              <ReactECharts option={item.genChart && JSON.parse(item.genChart ?? '{}')} />
+            </Card>
           </List.Item>
         )}
       />
