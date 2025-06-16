c7199c2de283bb3152a3cea1b08a322c2afd36ee
Octaver <Octaver2131@outlook.com>
2025-06-16 08:43:39 +0800

    my-chart-page

---
+++ src/pages/MyChart/index.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+
+/**
+ * 我的图表页面
+ * @constructor
+ */
+const MyChartPage: React.FC = () => {
+
+
+  return (
+    <div className="my-chart-page">
+
+    </div>
+  );
+};
+export default MyChartPage;
