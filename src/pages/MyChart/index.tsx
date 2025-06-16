d6a051660f0242f6be1ba9f452508f4fbd95b9fa
Octaver <Octaver2131@outlook.com>
2025-06-16 09:01:15 +0800

    my-chart-page 展示

--- src/pages/MyChart/index.tsx
+++ src/pages/MyChart/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { listMyChartByPageUsingPOST } from '@/services/yubi/chartController';
+import { message } from 'antd';
 
 
 /**
@@ -7,10 +9,39 @@ import React, { useState } from 'react';
  */
 const MyChartPage: React.FC = () => {
 
+  const initSearchParams = {
+    pageSize: 12,
+  }
+
+  const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({...initSearchParams});
+  const [chartList, setChartList] = useState<API.Chart>();
+  const [total, setTotal] = useState<number>(0);
+
+  const logData = async() => {
+    try {
+      const res = await listMyChartByPageUsingPOST(searchParams);
+      if (res.data) {
+        setChartList(res.data.records ?? []);
+        setTotal(res.data.total?? 0);
+      } else {
+        message.error(' 获取我的数据失败.');
+      }
+    } catch (e: any) {
+      message.error(' 获取我的数据失败.' + e.message);
+    }
+  }
+
+  useEffect(() => {
+    logData();
+  }, [searchParams]);
 
   return (
     <div className="my-chart-page">
-
+      数据列表
+      { JSON.stringify(chartList) }
+      <br/>
+      总数
+      { total }
     </div>
   );
 };
