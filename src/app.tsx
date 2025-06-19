8591852f8666d124df856f91ce658869323d7efa
Octaver <Octaver2131@outlook.com>
2025-06-19 08:44:18 +0800

    客制化

--- src/app.tsx
+++ src/app.tsx
@@ -41,6 +41,7 @@ export async function getInitialState(): Promise<{
 // ProLayout 支持的api https://procomponents.ant.design/components/layout
 export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) => {
   return {
+    logo: '/favicon.ico',
     actionsRender: () => [<Question key="doc" />],
     avatarProps: {
       src: initialState?.currentUser?.userAvatar,
